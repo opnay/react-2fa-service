dd5b47ee87d6a8be6db88fc82c81f5fbfc18a55d
Kim In Seop <opnayn@gmail.com>
2019-06-23 23:05:20 +0900

    Root: Add Empty Card for login card
    Signed-off-by: Kim In Seop <opnayn@gmail.com>

--- src/components/screen/Root/index.tsx
+++ src/components/screen/Root/index.tsx
@@ -1,11 +1,14 @@
 import './styles.css';
 
 import React from 'react';
+import Card from '../../atoms/Card';
 
 const RootScreen = () => {
   return (
     <div className='main'>
-      <div />
+      <Card className='login'>
+        <span>TEST</span>
+      </Card>
     </div>
   );
 };
