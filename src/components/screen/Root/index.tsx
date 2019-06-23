0d5b812d59912217c35e282378f6fb6fe252a88e
Kim In Seop <opnayn@gmail.com>
2019-06-23 23:01:45 +0900

    Root: Add GoogleSignIn Button
    Signed-off-by: Kim In Seop <opnayn@gmail.com>

--- src/components/screen/Root/index.tsx
+++ src/components/screen/Root/index.tsx
@@ -2,12 +2,41 @@ import './styles.css';
 
 import React from 'react';
 import Card from '../../atoms/Card';
+import { FirebaseContext } from '../../../context/FirebaseContext';
+import firebase from 'firebase';
 
 const RootScreen = () => {
+  const FirebaseApp = React.useContext(FirebaseContext);
+
+  // Get Google Auth Provider
+  const GoogleAuthProvider = React.useMemo(() => {
+    FirebaseApp.auth().languageCode = 'kr';
+    return new firebase.auth.GoogleAuthProvider();
+  }, [FirebaseApp]);
+
+  // onClickGoogle Button
+  const onClickGoogle = React.useCallback(() => {
+    return FirebaseApp.auth()
+      .signInWithPopup(GoogleAuthProvider)
+      .then((val: firebase.auth.UserCredential) => {
+        if (!val.credential) {
+          throw new Error('Failed to Google Auth');
+        }
+
+        const token: firebase.auth.OAuthCredential = val.credential;
+        console.log(token);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, [FirebaseApp]);
+
   return (
     <div className='main'>
       <Card className='login'>
-        <span>TEST</span>
+        <button className='button' onClick={onClickGoogle}>
+          Google SignIn
+        </button>
       </Card>
     </div>
   );
