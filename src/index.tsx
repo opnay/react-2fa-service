32166e44be42ca822eead7d20e26443dfacc9e03
Kim In Seop <opnayn@gmail.com>
2019-06-23 23:00:10 +0900

    index: Add ProviderComponent
    - Give contexts=[wrapped context.provider...],
      will render all context providers
    
    Signed-off-by: Kim In Seop <opnayn@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import AppRouter from './router';
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
+type ProviderProps = {
+  contexts: Array<React.ComponentType<any>>;
+  children?: React.ReactNode;
+};
+
+const ProviderComponent: React.FC<ProviderProps> = (props: ProviderProps) => {
+  const { contexts, children = <Fragment /> } = props;
+
+  const rendered = React.useMemo(() => {
+    const rendred: React.ReactNode = contexts.reduce<React.ReactNode>(
+      (prev, CurContext) => <CurContext>{prev}</CurContext>,
+      children
+    );
+
+    return rendred || children;
+  }, [contexts, children]);
+
+  return <Fragment>{rendered}</Fragment>;
+};
+
+const ContextProviders: Array<React.ComponentType> = [];
+
+ReactDOM.render(
+  <ProviderComponent contexts={ContextProviders}>
+    <AppRouter />
+  </ProviderComponent>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
