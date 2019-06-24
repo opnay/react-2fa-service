4be67ceb9d3a412fbeaa27b2b21025f14f10b025
Kim In Seop <opnayn@gmail.com>
2019-06-25 00:25:54 +0900

    router: Add Main route path and empty screen component
    Signed-off-by: Kim In Seop <opnayn@gmail.com>

---
+++ src/components/screen/Main/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Fragment } from 'react';
+
+const Main: React.FC = (props) => {
+  return <Fragment />;
+};
+
+export default Main;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,11 +3,28 @@ import {
   BrowserRouter,
   Switch,
   Route,
-  RouteComponentProps
+  RouteComponentProps,
+  RouteProps,
+  Redirect
 } from 'react-router-dom';
 import { hot } from 'react-hot-loader/root';
 
 import RootScreen from '../components/screen/Root';
+import { FirebaseContext } from '../context/FirebaseContext';
+import { APP_PATH } from './path';
+import Main from '../components/screen/Main';
+
+const PrivateRoute = (props: RouteProps) => {
+  // Incorrect
+  const FirebaseApp = React.useContext(FirebaseContext);
+
+  if (!FirebaseApp.auth().currentUser) {
+    return <Redirect to={APP_PATH.ROOT} />;
+  }
+
+  // Correct
+  return <Route {...props} />;
+};
 
 const NotFound = (props: RouteComponentProps) => {
   return <div>404 Not Found</div>;
@@ -17,7 +34,10 @@ const AppRouter = (props: {}) => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' component={RootScreen} />
+        <PrivateRoute exact path={APP_PATH.MAIN} component={Main} />
+
+        {/* Base Route */}
+        <Route exact path={APP_PATH.ROOT} component={RootScreen} />
         <Route render={NotFound} />
       </Switch>
     </BrowserRouter>
