7362d1b55323923c8d72dcdc18326b78efefdacd
Kim In Seop <opnayn@gmail.com>
2019-06-25 01:24:30 +0900

    utils: Add LocalStorageManager
    Signed-off-by: Kim In Seop <opnayn@gmail.com>

---
+++ src/utils/browser-support/LocalStorageManager.ts
@@ -0,0 +1,31 @@
+export class LocalStorageManager {
+  private readonly _key: string;
+
+  constructor(storageKey: string) {
+    this._key = storageKey;
+  }
+
+  get key(): string {
+    return this._key;
+  }
+
+  get value(): string {
+    const data = localStorage.getItem(this._key) || '';
+
+    // sometimes saved 'undefined'
+    if (data === 'undefined') {
+      localStorage.removeItem(this._key);
+      return '';
+    }
+
+    return data;
+  }
+
+  set value(value: string) {
+    if (!value || value === 'undefined') {
+      localStorage.removeItem(this._key);
+    } else {
+      localStorage.setItem(this._key, value);
+    }
+  }
+}
