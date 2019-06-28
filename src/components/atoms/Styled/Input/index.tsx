7e42248120116d96c9feeb368d94292754ca2058
Kim In Seop <opnayn@gmail.com>
2019-06-28 12:57:59 +0900

    atoms/Styled: Use React.forwardRef
    Signed-off-by: Kim In Seop <opnayn@gmail.com>

--- src/components/atoms/Styled/Input/index.tsx
+++ src/components/atoms/Styled/Input/index.tsx
@@ -7,8 +7,10 @@ export type InputProps = React.DetailedHTMLProps<
   HTMLInputElement
 >;
 
-export const Input = (props: InputProps) => {
-  const { className } = props;
-  const cn = React.useMemo(() => ['input', className].join(' '), [className]);
-  return <input {...props} className={cn} />;
-};
+export const Input = React.forwardRef(
+  (props: InputProps, ref: React.Ref<HTMLInputElement>) => {
+    const { className } = props;
+    const cn = React.useMemo(() => ['input', className].join(' '), [className]);
+    return <input {...props} className={cn} ref={ref} />;
+  }
+);
