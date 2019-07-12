8e709a14aef3dc8e08499224e5576f6265289b72
Kim In Seop <opnayn@gmail.com>
2019-07-12 21:27:45 +0900

    Main: Update Layout
    Signed-off-by: Kim In Seop <opnayn@gmail.com>

--- src/components/screen/Main/index.tsx
+++ src/components/screen/Main/index.tsx
@@ -1,14 +1,25 @@
 import './styles.css';
+import IconPlusCircle from '../../../assets/icons/PlusCircle.svg';
 
-import React from 'react';
+import React, { Fragment } from 'react';
 import TokenItem from './components/TokenItem';
 import TokenForm from './components/TokenForm';
 import { FirebaseContext } from '../../../context/FirebaseContext';
+import { Button } from '../../atoms/Styled';
+import Modal from '../../atoms/Modal';
+import { useToggle } from '../../../utils/react-support/Hook';
+import Card from '../../atoms/Card';
+import UserInfo from './components/UserInfo';
 
 type TokenObject = { [x: string]: string };
 
 const Main: React.FC = () => {
   const Firebase = React.useContext(FirebaseContext);
+  const [visible, toggleVisisble] = useToggle();
+  const onClickPlus = React.useCallback(() => toggleVisisble(), [
+    toggleVisisble
+  ]);
+
   // Token
   const [tokenObj, setTokenObj] = React.useState<TokenObject>({});
   const doc = React.useMemo(() => {
@@ -69,10 +80,20 @@ const Main: React.FC = () => {
   ));
 
   return (
-    <div className='content'>
-      <div className='bottom-line'>{renderTokens}</div>
-      <TokenForm onSubmit={onSubmit} />
-    </div>
+    <Fragment>
+      <div className='content'>
+        <UserInfo />
+        <div className='list-content'>{renderTokens}</div>
+      </div>
+      <Button className='add-token' onClick={onClickPlus}>
+        <img src={IconPlusCircle} alt='plus circle' />
+      </Button>
+      <Modal visible={visible}>
+        <Card className='input-form'>
+          <TokenForm onSubmit={onSubmit} />
+        </Card>
+      </Modal>
+    </Fragment>
   );
 };
 
