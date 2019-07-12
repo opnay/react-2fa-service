e6cf785cf8d53efb7be3c1fc1b82aacb1c1eda36
Kim In Seop <opnayn@gmail.com>
2019-07-13 03:57:06 +0900

    Main: Move logic of load firestore secret data to react-support/Hook
    Signed-off-by: Kim In Seop <opnayn@gmail.com>

--- src/components/screen/Main/index.tsx
+++ src/components/screen/Main/index.tsx
@@ -4,42 +4,24 @@ import IconPlusCircle from '../../../assets/icons/PlusCircle.svg';
 import React, { Fragment } from 'react';
 import TokenItem from './components/TokenItem';
 import TokenForm from './components/TokenForm';
-import { FirebaseContext } from '../../../context/FirebaseContext';
 import { Button } from '../../atoms/Styled';
 import Modal from '../../atoms/Modal';
-import { useToggle } from '../../../utils/react-support/Hook';
+import {
+  useToggle,
+  useFirestoreSecret
+} from '../../../utils/react-support/Hook';
 import Card from '../../atoms/Card';
 import UserInfo from './components/UserInfo';
 import { TokenType } from '../../../types/2fa-service/secret-token';
 
-type TokenObject = Array<TokenType>;
-
 const Main: React.FC = () => {
-  const Firebase = React.useContext(FirebaseContext);
   const [visible, toggleVisisble] = useToggle();
   const onClickVisible = React.useCallback(() => toggleVisisble(), [
     toggleVisisble
   ]);
 
   // Token
-  const [tokens, setTokens] = React.useState<TokenObject>([]);
-  const collection = React.useMemo(
-    () =>
-      Firebase.firestore()
-        .collection('2fa-service')
-        .doc(Firebase.auth().currentUser!.uid)
-        .collection('secrets'),
-    [Firebase]
-  );
-  const loadTokens = React.useCallback(() => {
-    collection.get().then((snapshot) => {
-      const data: TokenObject = snapshot.docs.map(
-        (it) => it.data() as TokenType
-      );
-      setTokens(data);
-    });
-  }, [collection]);
-  React.useEffect(() => loadTokens(), []);
+  const [tokens, collection, loadTokens] = useFirestoreSecret();
 
   // Callback
   const onSubmit = React.useCallback(
--- src/utils/react-support/Hook.ts
+++ src/utils/react-support/Hook.ts
@@ -1,4 +1,8 @@
-import { useState, useCallback } from 'react';
+import firebase from 'firebase/app';
+
+import { useState, useCallback, useContext, useMemo, useEffect } from 'react';
+import { TokenType } from '../../types/2fa-service/secret-token';
+import { FirebaseContext } from '../../context/FirebaseContext';
 
 export type HookToggle = [boolean, (s?: boolean) => void];
 
@@ -9,4 +13,41 @@ export function useToggle(defaultState: boolean = false): HookToggle {
     [state, setState]
   );
   return [state, setToggle];
+}
+
+export type HookFirestoreSecret = [
+  SecretType,
+  firebase.firestore.CollectionReference,
+  () => void
+];
+export type SecretType = Array<TokenType>;
+
+export function useFirestoreSecret(): HookFirestoreSecret {
+  const Firebase = useContext(FirebaseContext);
+  const [tokens, setTokens] = useState<SecretType>([]);
+
+  // Get firestore collection
+  const collection = useMemo(
+    () =>
+      Firebase.firestore()
+        .collection('2fa-service')
+        .doc(Firebase.auth().currentUser!.uid)
+        .collection('secrets'),
+    [Firebase]
+  );
+
+  // Load Secrets
+  const loadSecrets = useCallback(() => {
+    collection.get().then((snapshot) => {
+      const data: SecretType = snapshot.docs.map(
+        (it) => it.data() as TokenType
+      );
+      setTokens(data);
+    });
+  }, [collection]);
+
+  // Initial Loading
+  useEffect(() => loadSecrets(), []);
+
+  return [tokens, collection, loadSecrets];
 }
