39524b6a0141a29c910d3f79a8a1ed98e63bc547
Kim In Seop <opnayn@gmail.com>
2019-08-06 18:33:45 +0900

    utils: firebase: Enable Loading until onAuthStateChanged
    Signed-off-by: Kim In Seop <opnayn@gmail.com>

--- src/utils/firebase/Authentication.ts
+++ src/utils/firebase/Authentication.ts
@@ -1,5 +1,6 @@
 import { useContext, useState, useEffect } from 'react';
 import { FirebaseContext } from '../../context/FirebaseContext';
+import { LoadingContext } from '../../context/LoadingContext';
 
 export enum AuthState {
   NONE,
@@ -8,18 +9,24 @@ export enum AuthState {
 }
 
 export const isSignedIn = (): AuthState => {
+  const { toggleLoading } = useContext(LoadingContext);
   const FirebaseApp = useContext(FirebaseContext);
   const [state, setState] = useState(AuthState.NONE);
 
   useEffect(() => {
+    toggleLoading(true, '로그인 확인중...');
     FirebaseApp.auth().onAuthStateChanged((user) => {
       if (user) {
         setState(AuthState.SIGN_IN);
       } else {
         setState(AuthState.SIGN_OUT);
       }
+
+      toggleLoading(false);
     });
-  });
+
+    return () => toggleLoading(false);
+  }, []);
 
   return state;
 };
